adbd168fa35cf961635393564776891e2d519c72
Mohamed Picualt <mpicault2013@fau.edu>
2021-05-08 21:11:11 -0400

    how variables are declare and initialize in js

---
+++ Lesson 2/sandbox.js
@@ -0,0 +1,21 @@
+//To declare or initialize variables you use the keyword let before the name and assignment
+let age = 24;
+let year = 2019;
+
+//in order to output multiple values onto the console separare each variable with a comma 
+console.log(age, year);
+
+//console.log displays values on the console of a web browser
+console.log(age);
+
+//const prevents a variables value from being changed
+//attemping to overrider a variable that is const protected will result in a 
+//TypeError: Attempted to assign to readonly property.
+const points = 100;
+
+console.log(points);
+
+//var is older keyword to declare or initalilizing variables before let 
+var score = 45;
+
+console.log(score);
